3e2906f4d94c6a91a22e371a4efce010925a2a7b
Chris Junker <elevatemedfit@gmail.com>
2019-09-17 20:52:01 +0000

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -21,27 +21,4 @@ class TextAnalyzer
   def most_used_letter
     result = text.gsub(/[^a-z]/, '').chars.group_by(&:itself).map {|letter, list| [letter, list.count]}.max_by(&:last)
   end
-
-# most_common_letter("green eggs and ham")
-
-  #   s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
-  #   arr = s1.split('')
-  #   arr1 = arr.uniq
-  #   arr2 = {}
-  #
-  #   arr1.map do |c|
-  #     arr2[c] =  arr.count(c)
-  #   end
-  #
-  #   biggest = { arr2.keys.first => arr2.values.first }
-  #
-  #   arr2.each do |key, value|
-  #     if value > biggest.values.first
-  #       biggest = {}
-  #       biggest[key] = value
-  #     end
-  #   end
-  #
-  #   biggest
-  # end
 end
