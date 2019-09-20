f38409db4c77160a846a7123335daaeafb27552f
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-09-19 23:04:48 -0700

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @count = @analyzed_text.count_of_words
+    @count_vowels = @analyzed_text.count_of_vowels
+    @count_consonants = @analyzed_text.count_of_consonants
+    @most_letters_used_one = @analyzed_text.most_used_letter.keys.first.upcase
+    @most_letters_used_two = @analyzed_text.most_used_letter.values.first
+
 
     erb :results
   end
